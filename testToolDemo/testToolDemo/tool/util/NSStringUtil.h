374b5c485d581bb9e6b257f6e855e7009916e9cd
xiancai <xiancai@focusoo.com>
2016-07-07 13:28:21 +0800

    [ADD]校验手机号，邮箱和车牌号工具类

---
+++ testToolDemo/testToolDemo/tool/util/NSStringUtil.h
@@ -0,0 +1,36 @@
+//
+//  NSStringUtil.h
+//  testToolDemo
+//
+//  Created by 段贤才 on 16/7/7.
+//  Copyright © 2016年 volientDuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSStringUtil : NSObject
+/**
+ *  校验邮箱格式
+ *
+ *  @param email 需要校验的邮箱
+ *
+ *  @return 返回结果
+ */
++(BOOL)isValidateEmail:(NSString *)email;
+/**
+ *  校验手机号格式
+ *
+ *  @param mobile 手机号
+ *
+ *  @return 结果
+ */
++(BOOL) isValidateMobile:(NSString *)mobile;
+/**
+ *  校验车牌号
+ *
+ *  @param carNo 车牌号
+ *
+ *  @return 结果
+ */
++(BOOL) isvalidateCarNo:(NSString*)carNo;
+@end
---
+++ testToolDemo/testToolDemo/tool/util/NSStringUtil.m
@@ -0,0 +1,39 @@
+//
+//  NSStringUtil.m
+//  testToolDemo
+//
+//  Created by 段贤才 on 16/7/7.
+//  Copyright © 2016年 volientDuan. All rights reserved.
+//
+
+#import "NSStringUtil.h"
+
+@implementation NSStringUtil
+/*邮箱验证 MODIFIED BY HELENSONG*/
++(BOOL)isValidateEmail:(NSString *)email
+{
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:email];
+}
+
+/*手机号码验证 MODIFIED BY HELENSONG*/
++(BOOL) isValidateMobile:(NSString *)mobile
+{
+    //手机号以13， 15，18开头，八个 \d 数字字符
+    NSString *phoneRegex = @"^((13[0-9])|(15[^4,\\D])|(18[0,0-9]))\\d{8}$";
+    NSPredicate *phoneTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",phoneRegex];
+    //    NSLog(@"phoneTest is %@",phoneTest);
+    return [phoneTest evaluateWithObject:mobile];
+}
+
+/*车牌号验证 MODIFIED BY HELENSONG*/
++(BOOL) isvalidateCarNo:(NSString*)carNo
+{
+    NSString *carRegex = @"^[A-Za-z]{1}[A-Za-z_0-9]{5}$";
+    NSPredicate *carTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",carRegex];
+    NSLog(@"carTest is %@",carTest);
+    return [carTest evaluateWithObject:carNo];
+} 
+
+@end
