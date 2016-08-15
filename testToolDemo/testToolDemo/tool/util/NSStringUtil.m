6a3f039f0982a500d75cf0f3325e81e60176a84a
xiancai <1107639248@qq.com>
2016-08-15 10:05:53 +0800

    -[FIX]更新手机号校验正则表达式

--- testToolDemo/testToolDemo/tool/util/NSStringUtil.m
+++ testToolDemo/testToolDemo/tool/util/NSStringUtil.m
@@ -20,11 +20,51 @@
 /*手机号码验证*/
 +(BOOL) isValidateMobile:(NSString *)mobile
 {
-    //手机号以13， 15，18开头，八个 \d 数字字符
-    NSString *phoneRegex = @"^((13[0-9])|(15[^4,\\D])|(18[0,0-9]))\\d{8}$";
-    NSPredicate *phoneTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",phoneRegex];
-    //    NSLog(@"phoneTest is %@",phoneTest);
-    return [phoneTest evaluateWithObject:mobile];
+    if (mobile.length != 11)
+    {
+        return NO;
+    }
+    /**
+     * 手机号码:
+     * 13[0-9], 14[5,7], 15[0, 1, 2, 3, 5, 6, 7, 8, 9], 17[6, 7, 8], 18[0-9], 170[0-9]
+     * 移动号段: 134,135,136,137,138,139,150,151,152,157,158,159,182,183,184,187,188,147,178,1705
+     * 联通号段: 130,131,132,155,156,185,186,145,176,1709
+     * 电信号段: 133,153,180,181,189,177,1700
+     */
+    NSString *MOBILE = @"^1(3[0-9]|4[57]|5[0-35-9]|8[0-9]|70)\\d{8}$";
+    /**
+     * 中国移动：China Mobile
+     * 134,135,136,137,138,139,150,151,152,157,158,159,182,183,184,187,188,147,178,1705
+     */
+    NSString *CM = @"(^1(3[4-9]|4[7]|5[0-27-9]|7[8]|8[2-478])\\d{8}$)|(^1705\\d{7}$)";
+    /**
+     * 中国联通：China Unicom
+     * 130,131,132,155,156,185,186,145,176,1709
+     */
+    NSString *CU = @"(^1(3[0-2]|4[5]|5[56]|7[6]|8[56])\\d{8}$)|(^1709\\d{7}$)";
+    /**
+     * 中国电信：China Telecom
+     * 133,153,180,181,189,177,1700
+     */
+    NSString *CT = @"(^1(33|53|77|8[019])\\d{8}$)|(^1700\\d{7}$)";
+    
+    
+    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
+    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
+    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
+    
+    if (([regextestmobile evaluateWithObject:mobile] == YES)
+        || ([regextestcm evaluateWithObject:mobile] == YES)
+        || ([regextestct evaluateWithObject:mobile] == YES)
+        || ([regextestcu evaluateWithObject:mobile] == YES))
+    {
+        return YES;
+    }
+    else
+    {
+        return NO;
+    }
 }
 
 /*车牌号验证*/
